767bcb40c9f8d5d6f104f3f2aa7af6c28e13d9a7
Reinsch94 <alexandre.reinsch@gmail.com>
2021-04-20 11:43:01 +0700

    ✨ Add Nebular styles + NbLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NbThemeModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NbThemeModule.forRoot()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
