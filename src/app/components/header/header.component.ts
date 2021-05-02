3d9cd483e4d0b3159c67943d99c51c8e13b4dcec
Reinsch94 <alexandre.reinsch@gmail.com>
2021-05-02 11:15:52 +0700

    ✨ Add login / logout button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/pages/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  isUserLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public auth: AngularFireAuth
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.router.navigate(['/pages/login']);
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
