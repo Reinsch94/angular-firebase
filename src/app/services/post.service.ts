f51784d1260e0c47776c9124b270daa4b0b4c64c
Reinsch94 <alexandre.reinsch@gmail.com>
2021-05-04 11:49:50 +0700

    ✨ CRUD posts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Post } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
-  constructor() {}
+  constructor(private firestore: AngularFirestore) {}
+
+  getPosts() {
+    return this.firestore.collection('posts').snapshotChanges();
+  }
+
+  createPost(post: Post) {
+    return this.firestore.collection('posts').add(post);
+  }
+
+  updatePost(post: Post) {
+    return this.firestore.doc('posts/' + post.id).update(post);
+  }
+
+  deletePost(postId: string) {
+    return this.firestore.doc('posts/' + postId).delete();
+  }
 }
