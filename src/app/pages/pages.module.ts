e6e4d55667469e3a9f6c20f6c2a4dfd6e0d81cd6
Reinsch94 <alexandre.reinsch@gmail.com>
2021-05-04 11:13:27 +0700

    ✨ Create post component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -19,6 +19,7 @@ import {
   HeaderComponent,
   SidebarComponent,
 } from '../components';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   imports: [
@@ -40,6 +41,7 @@ import {
     HomeComponent,
     LoginComponent,
     RegisterComponent,
+    PostComponent,
   ],
 })
 export class PagesModule {}
